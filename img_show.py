61edf63fe89e8cf46a0530fd53a37360227fa22a
ei1994 <2393058804@qq.com>
2017-10-25 15:25:24 +0800

    Update img_show.py

--- img_show.py
+++ img_show.py
@@ -19,6 +19,11 @@ cv2.waitKey(1)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 
+‘’‘
+注意:彩色图像使用 OpenCV 加载时是 BGR 模式。但是 Matplotib 是 RGB
+模式。所以彩色图像如果已经被 OpenCV 读取,那它将不会被 Matplotib 正
+确显示。
+’‘’
 ## 3.plt to show
 #plt.imshow(img,cmap='gray')
 #plt.xticks([]), plt.yticks([])
