0c6ba96e1fd6e234d305633eb260cd5913c2dd8b
ei1994 <2393058804@qq.com>
2017-10-26 17:18:28 +0800

    Update thresholding2.py

--- thresholding2.py
+++ thresholding2.py
@@ -17,6 +17,12 @@ C - It is just a constant which is subtracted from the mean or weighted mean cal
 img = cv2.imread('images/32.jpg',0)
 # 中值滤波
 img = cv2.medianBlur(img,5)
+# 高斯滤波
+blur = cv2.GaussianBlur(img,(5,5),0)
+# 指定卷积核滤波
+kernel = np.ones((5,5),np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
+
 ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 #11 为 Block size, 2 为 C 值
 th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
