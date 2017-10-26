fa3e1b9ba01cba34bc7e8eb2b1329a617f63790b
ei1994 <2393058804@qq.com>
2017-10-26 17:10:06 +0800

    adaptive threwsholding

---
+++ thresholding2.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import matplotlib.pylab as plt
+import scipy.misc as misc
+
+'''
+Adaptive Method - It decides how thresholding value is calculated.
+
+    cv2.ADAPTIVE_THRESH_MEAN_C : threshold value is the mean of neighbourhood area.
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C : threshold value is the weighted sum of neighbourhood values
+                                where weights are a gaussian window.
+
+Block Size - It decides the size of neighbourhood area.
+
+C - It is just a constant which is subtracted from the mean or weighted mean calculated.
+'''
+img = cv2.imread('images/32.jpg',0)
+# 中值滤波
+img = cv2.medianBlur(img,5)
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+#11 为 Block size, 2 为 C 值
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                            cv2.THRESH_BINARY,11,2)
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+          'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
