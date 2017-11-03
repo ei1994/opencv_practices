76d47dc7851e48c90a080199dcad55bfdbc5a2e9
WangRuojing <2393058804@qq.com>
2017-11-03 22:38:54 +0800

    OCR数字识别

---
+++ svm.py
@@ -0,0 +1,78 @@
+# SVM
+
+import cv2
+import numpy as np
+SZ=20
+bin_n = 16 # Number of bins
+affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
+def deskew(img):
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    img = cv2.warpAffine(img,M,(SZ, SZ),flags=affine_flags)
+    return img
+def hog(img):
+    gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+    gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+    mag, ang = cv2.cartToPolar(gx, gy)
+    bins = np.int32(bin_n*ang/(2*np.pi))
+    # quantizing binvalues in (0...16)
+    bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
+    mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
+    hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)
+    # hist is a 64 bit vector
+    return hist
+img = cv2.imread('images/digits.png',0)
+cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
+# First half is trainData, remaining is testData
+train_cells = [ i[:50] for i in cells ]
+test_cells = [ i[50:] for i in cells]
+###### Now training ########################
+#deskewed = [map(deskew,row) for row in train_cells]
+##deskewed = map(lambda  row: deskew ,train_cells)
+#hogdata = [map(hog,row) for row in deskewed]
+#hogdata = list(map(lambda row: hog, deskewed))
+#hogdata = list(hogdata)
+deskewed = []
+for row in train_cells:
+    for img in row:
+        res = deskew(img)
+        deskewed.append(res)
+hogdata = []
+for row in deskewed:
+        res = hog(row)
+        hogdata.append(res)
+
+trainData = np.float32(hogdata).reshape(-1,64)
+responses = np.int32(np.repeat(np.arange(10),250)[:,np.newaxis])
+
+svm = cv2.ml.SVM_create()
+svm.setType(cv2.ml.SVM_C_SVC)
+svm.setKernel(cv2.ml.SVM_LINEAR)
+svm.setTermCriteria((cv2.TERM_CRITERIA_COUNT, 100, 1.e-06))
+svm.train(trainData, cv2.ml.ROW_SAMPLE, responses)
+
+# 支持向量  
+vec = svm.getSupportVectors()  
+#print(vec) 
+#svm.save('svm_data.dat')
+###### Now testing ########################
+
+deskewed = []
+for row in test_cells:
+    for img in row:
+        res = deskew(img)
+        deskewed.append(res)
+hogdata = []
+for row in deskewed:
+        res = hog(row)
+        hogdata.append(res)
+testData = np.float32(hogdata).reshape(-1,bin_n*4)
+result = svm.predict(testData)
+####### Check Accuracy ########################
+mask = result[1]==responses
+correct = np.count_nonzero(mask)
+print (correct*100.0/result[1].size)
